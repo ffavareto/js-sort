9890cc8b3f588b112911e3ded35a49d6d49a349c
Lucas Santos <lucasfavaretosantos98@gmail.com>
2023-08-09 20:46:36 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const numeros = [1, 2, 3, 101, 23, 45, 23, 3, 300];
 
-// const numerosOrdenados = numeros.sort((a, b) => {
-//     if (a == b) {
-//         return 0;
-//     }
+const numerosOrdenados = numeros.sort((a, b) => {
+    if (a == b) {
+        return 0;
+    }
 
-//     if (a < b) {
-//         return -1;
-//     }
+    if (a < b) {
+        return -1;
+    }
 
-//     if (a > b) {
-//         return 1;
-//     }
-// });
+    if (a > b) {
+        return 1;
+    }
+});
 
 const numerosOrdenados = numeros.sort((a, b) => a - b);
 
